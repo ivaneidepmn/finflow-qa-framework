7da04bad8731bf9c9c15ad8cd0a15ca40e707056
Ivaneide Pires Monteiro do Nascimento <ivapmn@gmail.com>
2025-12-29 01:30:29 -0300

    Update LoginPage.js

--- automation/cypress/support/pages/LoginPage.js
+++ automation/cypress/support/pages/LoginPage.js
@@ -1,1 +1,8 @@
 
+class LoginPage {
+  visitar() { cy.visit('/login'); }
+  preencherUsuario(usuario) { cy.get('#username').type(usuario); }
+  preencherSenha(senha) { cy.get('#password').type(senha); }
+  submeter() { cy.get('#loginButton').click(); }
+}
+export default new LoginPage();
